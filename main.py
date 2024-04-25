80dd445d73bcf2eda6ffe7f0117ce75fe2c860f1
Matheus Esteves <matheus.mat160805@gmail.com>
2024-04-25 14:34:36 -0300

    finalização jogo da forca

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
+import random
+
 print("------------------------------")
 print("Bem vindo ao jogo da forca")
 print("------------------------------")
 
 
 
-sorteada = "frango assado"
+lista_de_palavras = ["frango assado", "maça", "banana", "amendoim", "batata frita", "chocolate", "carne assada", "arroz", "feijao", "abobrinha", "couve flor", "alface", "tomate"]
+sorteada = random.choice(lista_de_palavras)
 erros = 0
-
+chutadas = " "
 
 segredo = ""
+
 for caractere in sorteada:
-    if caractere == " ":
-        segredo = segredo + "   "
+    if caractere in chutadas:
+            segredo = segredo + caractere + " "
     else:
-        segredo = segredo + "_ " 
-    
+            segredo = segredo + "_ "
 
 
-print(f'{segredo} \n Número de erros: {erros}') #_ _ _ _ _   _ _ _ _ _ _
+while erros < 6 and "_" in segredo:
+    print(f'{segredo} \n Número de erros: {erros}') 
+    print(f"Letras chutadas: {', '.join(chutadas[1:])}")
+    letra_usuario = str(input('Chute uma letra: ')).lower().strip()
+    chutadas = chutadas + letra_usuario
 
-letra_usuario = str(input('Chute uma letra: ')).lower().strip()
-
-if letra_usuario in sorteada:
     segredo = ""
     for caractere in sorteada:
-        if caractere == letra_usuario:
-            segredo = segredo + letra_usuario + " "
-        elif caractere == " ":
-            segredo = segredo + "   "
+        if caractere in chutadas:
+            segredo = segredo + caractere + " "
         else:
             segredo = segredo + "_ "
     
-else:
-    erros = erros + 1
+    if not letra_usuario in sorteada:
+         erros = erros + 1
 
-print(f'{segredo} \n Número de erros: {erros}')
+if erros == 6:
+     print(f"Você foi enforcado. A palavra era {sorteada}")
+else:
+     print(f"Parabéns, você acertou a palavra {sorteada}")
